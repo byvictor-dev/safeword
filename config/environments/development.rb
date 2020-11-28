62dba8bb02c4b418b74d598725631fb71df5420c
jonathan@platform45.com <jonathan@platform45.com>
2020-11-28 12:16:20 +0200

    Removed unnecessary spec. Moved SMTP configs to production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,22 +32,13 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.preview_path = Rails.root.join('spec', 'mailers', 'previews')
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.default_url_options = { host: ENV['APPLICATION_HOST'] }
-  config.action_mailer.smtp_settings = {
-    address: "smtp-relay.sendinblue.com",
-    port: 587,
-    domain: "smtp-relay.sendinblue.com",
-    user_name: ENV['SENDINBLUE_LOGIN'],
-    password: ENV['SENDINBLUE_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
